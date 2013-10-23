fbf35bcc085406e3c435792a4922418c919f8c66
Tomasz Grabowski <belike81@gmail.com>
2013-10-23 22:34:43 +0200

    Added a country to the geo data table

--- db/migrate/20130702194416_create_geo_data.rb
+++ db/migrate/20130702194416_create_geo_data.rb
@@ -1,6 +1,7 @@
 class CreateGeoData < ActiveRecord::Migration
   def change
     create_table :locationer_geo_data do |t|
+      t.string :country
       t.string :city_name
       t.string :state
       t.float :latitude
